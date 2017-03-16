7ed1d4f22a289119d798b48c0c03c74d4100ceba
Digithun <michael@digithun.co.th>
2017-03-16 16:06:01 +0700

    Fix iOS project build

--- index.ios.js
+++ index.ios.js
@@ -12,7 +12,7 @@ import {
   View
 } from 'react-native';
 
-export default class clogii extends Component {
+export default class Clogii extends Component {
   render() {
     return (
       <View style={styles.container}>
@@ -50,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('clogii', () => clogii);
+AppRegistry.registerComponent('Clogii', () => Clogii);
